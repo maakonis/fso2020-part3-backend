ee7da5e4ed4478234c0b800ab4a683849f7a574d
Mareks Zevalds <zevaldsmareks@gmail.com>
2020-04-20 16:50:17 +0100

    Update local server port

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
 
+
 morgan.token('content', (req, res) => JSON.stringify(req.body))
 
+app.use(express.static('build'))
 app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
@@ -85,7 +87,7 @@ app.post('/api/persons', (req, res) => {
   res.json(person)
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
