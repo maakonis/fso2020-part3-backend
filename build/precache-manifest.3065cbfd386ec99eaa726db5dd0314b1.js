cfcfb4896934e4612f686927fc929c54abdc9e45
Mareks Zevalds <zevaldsmareks@gmail.com>
2020-04-20 16:48:47 +0100

    Migrate frontend production build to the backend.

---
+++ build/precache-manifest.3065cbfd386ec99eaa726db5dd0314b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab46c689cf99328c3ab3990d1be5d5e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "056bbd0d7983a304b0f9",
+    "url": "/static/css/main.2b6b4a07.chunk.css"
+  },
+  {
+    "revision": "fb4cca7c826df130d13f",
+    "url": "/static/js/2.91d2cd84.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.91d2cd84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "056bbd0d7983a304b0f9",
+    "url": "/static/js/main.5e2136d5.chunk.js"
+  },
+  {
+    "revision": "2707f5a550e5129b2153",
+    "url": "/static/js/runtime-main.69f5a8ea.js"
+  }
+]);
