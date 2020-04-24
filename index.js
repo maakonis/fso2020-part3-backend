487521e6e3b576dded319cb7ae97f9269f47cb82
Mareks <zevaldsmareks@gmail.com>
2020-04-24 19:22:31 +0100

    Complete Part 3 exercises up to 3.18.

--- index.js
+++ index.js
@@ -1,93 +1,125 @@
+//express is a node.js library that allows to setup the backend of the application
 const express = require('express')
 const app = express()
-const morgan = require('morgan')
+//dotenv enables elegant storage of environmental (security sensitive) variables to
+//be stored in a file for development.
+require('dotenv').config()
+//CORS allows to make HTTP requests outside of root domain
 const cors = require('cors')
+//mongoose is an easy to use library to interact with the MongoDB database service
+const mongoose = require('mongoose')
 
+//imports a model for that define the structure, form and location of database documents (entries)
+const Person = require('./models/note')
 
-morgan.token('content', (req, res) => JSON.stringify(req.body))
+//middleware (.use) is set-up prior to any routes in order to process/format requests
 
-app.use(express.static('build'))
+//cors middleware
 app.use(cors())
+//static build middleware specified that the front-end build version index.html is rendered at the root
+app.use(express.static('build'))
+//json-parser takes the JSON data of the request, transforms to JS object and attaches it as a body
+//property to the request object. Otherwise, request.body would be undefined.
 app.use(express.json())
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
+//middleware for the deprecated findByIdAndUpdate
+mongoose.set('useFindAndModify', false);
 
-let persons = [
-  {
-    name: "Arto Hellas",
-    number: "040-123456",
-    id: 1
-  },
-  {
-    name: "Ada Lovelace",
-    number: "39-123456",
-    id: 2
-  },
-  {
-    name: "Dan Abramov",
-    number: "39-123456-23222",
-    id: 3
-  },
-  {
-    name: "Mary Poppendieck",
-    number: "39-123456-34333",
-    id: 4
-  }
-]
+//following are the various routes the request will be processed
 
-app.get('/api/persons', (req, res) => {
-  res.json(persons)
+app.get('/api/persons', (request, response) => {
+  Person.find({}).then(entries => {
+    response.json(entries.map(entry => entry.toJSON()))
+  })
 })
 
-app.get('/info', (req, res) => {
-  const info =
-    `<div>
-      <p>Phonebook has info for ${persons.length} people.</p>
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(function (note) {
+      console.log(response.json(note))
+      return note ? response.json(note.toJSON()) : response.status(404).end();
+    })
+    .catch(error => {
+      console.log(error)
+      next(error)
+    })
+})
+
+app.get('/info', (request, response) => {
+
+  Person.countDocuments({}).then(count => {
+    console.log('count:', count)
+    response.send(
+      `<div>
+      <p>Phonebook has info for ${count} people.</p>
       <p>${new Date()}</p>
-    </div>`
-  res.send(info)
+      </div>`
+    )
+    })
+      .catch(error => next(error))
 })
 
-app.get('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  const person = persons.find(person => person.id === id)
-  
-  if (person) {
-    res.json(person)
-  } else {
-    res.status(404).end()
-  }
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => response.status(204).end())
+    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  persons = persons.filter(person => person.id !== id)
-  res.status(204).end()
-})
-
-app.post('/api/persons', (req, res) => {
-  const body = req.body
-  console.log('body', body)
+app.post('/api/persons', (request, response) => {
+  const body = request.body
 
   if (!body.name) {
-    return res.status(400).json({
+    return response.status(400).json({
       error: 'name entry missing'
     })
   } else if (!body.number) {
-    return res.status(400).json({
+    return response.status(400).json({
       error: 'number entry missing'
     })
   }
 
-  const person = {
+  const person = new Person({
     name: body.name,
     number: body.number,
     id: parseInt(Math.random() * 10000, 10)
-  }
-  persons = persons.concat(person)
-  res.json(person)
+  })
+
+  person.save().then(savedNote => response.json(savedNote.toJSON()))
 })
 
-const PORT = process.env.PORT || 5000
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedNote => response.json(updatedNote.toJSON()))
+    .catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+//error handling middleware must be positioned at the end of routes.
+//otherwise the request will be processed by the error handler,
+//program will end without going through the routes.
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+app.use(errorHandler)
+
+const PORT = process.env.PORT
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
---
+++ mongo.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose')
+
+const password = process.argv[2]
+
+const url =
+    `mongodb+srv://mareks-fso:${password}@cluster0-kwqsb.mongodb.net/phonebook-db?retryWrites=true&w=majority`
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const phoneSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+})
+
+const PhoneEntry = mongoose.model('PhoneEntry', phoneSchema)
+
+if (!process.argv[3]) {
+    PhoneEntry.find({}).then(result => {
+        console.log('phonebook:')
+        result.forEach(entry => console.log(entry))
+        mongoose.connection.close()
+    })
+} else {
+    const phoneEntry = new PhoneEntry({
+        name: process.argv[3],
+        number: process.argv[4]
+    })
+
+    phoneEntry.save().then(response => {
+        console.log('phoneentry saved!')
+        mongoose.connection.close()
+    })
+}
+
+
+
+
+
